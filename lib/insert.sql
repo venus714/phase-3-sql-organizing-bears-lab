7f0b07e4fd86a0178b6d41af5196491d9fb4ad3b
Student <venus.kabera@student.moringaschool.com>
2023-02-15 15:55:59 +0300

    completed

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ('Mr Chocolate','10','M','brown','relaxed','Yes');
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ('Rowdy','10','M','black','fire','Yes');
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ('Tabitha','8','F','white','social','Yes');
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ('Sergent Brown','15','M','red','Tensed','No');
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ('Melissa','2','F','maroon','playful','Yes');
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ('Grinch','5','M','white','silent','Yes');
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ('Wendy','7','F','black','loud','No');
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES (NULL,'5','F','white','lost','No');
